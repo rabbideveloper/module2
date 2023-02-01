a4a0a69cb9c87b6a1f52a78140969bf0835016c2
Rabbi Sheikh <mdrabbyrabby96@gmail.com>
2023-02-01 11:45:06 +0600

    Last Commit

--- variable.php
+++ variable.php
@@ -4,6 +4,39 @@
 
 $name = "Rabbi Sheikh";
 
+$question = "How are you?";
+
 $age = 21;
 
-echo "My name is ".$name. "Age is ".$age;
+//echo "My name is ".$name. "Age is ".$age;
+
+$word = 'age';
+
+//echo $$word;
+
+//echo "\n hello rabbi";
+
+//echo "My age is {$age}";
+
+echo "Hey {$name}. {$question}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
