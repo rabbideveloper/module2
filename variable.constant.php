3ed02f06a225123659be26e95e053326643ff372
Rabbi Sheikh <mdrabbyrabby96@gmail.com>
2023-02-01 12:14:02 +0600

    Add

---
+++ variable.constant.php
@@ -0,0 +1,31 @@
+<?php
+
+$name = "Rabbi Sheikh";
+
+echo $name;
+
+echo "\n";
+
+$name = "Siam Sheikh";
+
+echo $name;
+
+define('PI',3.1459);
+
+echo "\n";
+echo "Value of PI = ".PI;
+
+//$PI = 10;
+//
+//echo "\n {$PI}";
+
+echo "\n";
+
+//define('PI',100);
+
+echo PI;
+
+
+$constant = 'constant';
+echo "\n";
+echo "Value of PI = {$constant('PI')}";
