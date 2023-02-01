9d03819acb4a1418f774ffd57f7500764da64b53
Rabbi Sheikh <mdrabbyrabby96@gmail.com>
2023-02-01 21:54:15 +0600

    commit

--- if.php
+++ if.php
@@ -1,2 +1,15 @@
 <?php
 
+$a = 10;
+$b = 20;
+$c = 54;
+
+if ($a > $b){
+    if ($a > $c){
+        echo "A is large number";
+    }
+}elseif ($b > $c){
+    echo "B is large number";
+}else{
+    echo "C is large number";
+}
--- switch.php
+++ switch.php
@@ -1,2 +1,58 @@
 <?php
 
+//$day = "friday";
+//
+//switch ($day){
+//    case 'fsfds' :
+//        echo "Today is Friday";
+//        break;
+//    case 'saturday' :
+//        echo "Today is Saturday";
+//        break;
+//    case 'sunday' :
+//        echo "Today is Sunday";
+//        break;
+//    case 'monday' :
+//        echo "Today is monday";
+//        break;
+//    case 'tuesday' :
+//        echo "Today is Tuesday";
+//        break;
+//    case 'wednesday':
+//        echo "Today is Wednesday";
+//        break;
+//    case 'thursday' :
+//        echo "Today is Thursday";
+//        break;
+//    default :
+//        echo "Not a valid day enter";
+//}
+
+
+//$a = 1042324;
+//$b = 544;
+//$c = 5065464564564;
+//
+//switch (true){
+//    case $a > $b :
+//        switch ($a){
+//            case $a > $c :
+//                echo "A is large number";
+//        }   break;
+//    case $b > $c :
+//        echo "B is large number";
+//        break;
+//    default :
+//        echo "C is large number";
+//}
+
+
+//$a = 1;
+//switch ($a){
+//    case $a % 2 == 0 :
+//        echo "Even number";
+//        break;
+//    default :
+//        echo "Odd number";
+//}
+
