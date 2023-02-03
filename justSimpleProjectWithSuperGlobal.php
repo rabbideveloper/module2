f0f783f3e5ee44521c76f6b4e445b01cee486d45
Rabbi Sheikh <mdrabbyrabby96@gmail.com>
2023-02-04 01:03:11 +0600

    add

--- justSimpleProjectWithSuperGlobal.php
+++ justSimpleProjectWithSuperGlobal.php
@@ -1,2 +1,33 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple Project</title>
+</head>
+<body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+        Name: <input type="text" name="fname" >
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
 
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"]==="POST"){
+        // collect value of input field
+
+        $name = $_REQUEST['fname'];
+
+        if (empty($name)){
+            echo "Name is empty";
+        }else{
+            echo $name;
+        }
+
+    }
+
+?>
