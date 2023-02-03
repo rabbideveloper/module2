96abb6f48b072fa99b1f8e22de5d8be25b174d28
Rabbi Sheikh <mdrabbyrabby96@gmail.com>
2023-02-04 00:47:11 +0600

    add

---
+++ suparGlobal.php
@@ -0,0 +1,29 @@
+<?php
+
+$x = 75;
+$y = 25;
+
+//function addition()
+//{
+//    $GLOBALS['z'] =$GLOBALS['x'] + $GLOBALS['y'];
+//}
+//
+//addition();
+//echo $z;
+
+
+//==============PHP $_SERVER==============
+
+
+//echo $_SERVER['PHP_SELF'];
+//echo "\n";
+//echo $_SERVER['SERVER_NAME'];
+//echo "\n";
+//echo $_SERVER['HTTP_HOST'];
+//echo "\n";
+//echo $_SERVER['HTTP_REFERER'];
+//echo "\n";
+//echo $_SERVER['HTTP_USER_AGENT'];
+//echo "\n";
+//echo $_SERVER['SCRIPT_NAME'];
+
